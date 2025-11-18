900702a6a9b19a6fbc86806a2c4d4fd69f2e67ff
kitykatgirl <kamilord220@vp.pl>
2025-11-18 08:23:53 +0100

    added cheking of email and password functions and the find views

--- app/src/main/java/ph/me/klloginapp/MainActivity.java
+++ app/src/main/java/ph/me/klloginapp/MainActivity.java
@@ -1,12 +1,21 @@
 package ph.me.klloginapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +29,31 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        EditText email = findViewById(R.id.email);
+        EditText pass1 = findViewById(R.id.pass1);
+        EditText pass2 = findViewById(R.id.pass2);
+        Button btnZatwierdz = findViewById(R.id.btnZatwierdz);
+        TextView bottomtext = findViewById(R.id.bottomText);
+
+
+    }
+    public boolean CheckEmailValidity(EditText email, TextView errorTextView){
+        char[] emailArray = email.getText().toString().toCharArray();
+        if(Objects.equals(emailArray, '@')) {
+            return true;
+        }
+        else {
+            errorTextView.setText("Nieprawidlowy adres email");
+            return false;
+        }
+    }
+    public boolean CheckPasswordMatching(EditText first, EditText second, TextView errorTextView){
+        if (first.getText().toString().equals(second.getText().toString())){
+            return true;
+        }
+        else {
+            errorTextView.setText("Hasla sie roznia");
+            return false;
+        }
     }
 }
